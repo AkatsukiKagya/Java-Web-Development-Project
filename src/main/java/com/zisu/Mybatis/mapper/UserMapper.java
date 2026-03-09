14a42862d3e4434a1afe92e8a8c740d40b02434f
Aka <2937564051@qq.com>
2026-03-09 14:56:19 +0800

    26.3.9.2

--- src/main/java/com/zisu/Mybatis/mapper/UserMapper.java
+++ src/main/java/com/zisu/Mybatis/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.zisu.Mybatis.mapper;
 
 import com.zisu.Mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Map;
 
@@ -9,4 +10,6 @@ public interface UserMapper {
     public User getUserByName(String username);
     public User getUserByIdandName(String userid, String username);
     public User getUserBymap(Map<String, Object> map);
+    public User getUserByParamAnnotation(@Param("userid") String userid, @Param("username") String username);
+    public User getUserByUser(User user);
 }
